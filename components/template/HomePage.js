36b021afb839a99c7d3adcd09a2820960f5e4369
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-03-06 00:00:46 +0330

    Definitoin added

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,12 +1,14 @@
 import styles from './Homepage.module.css'
 import Banner from '../module/Banner'
 import Attributes from '../module/Attributes'
+import Definition from '../module/Definition'
 
 export default function HomePage() {
     return(
         <div className={styles.container}>
         <Banner />
         <Attributes />
+        <Definition />
         </div>
     )
 }
