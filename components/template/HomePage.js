464bb67234d7430da94d607c49d46de9e72c31b0
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-03-06 00:15:38 +0330

    Homepage finished

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -4,6 +4,8 @@ import Attributes from '../module/Attributes'
 import Definition from '../module/Definition'
 import Companies from '../module/Companies'
 import Instruction from '../module/Instructions'
+import Guide from '../module/Guide'
+import Restrictions from '../module/Restrictions'
 
 export default function HomePage() {
     return(
@@ -13,6 +15,8 @@ export default function HomePage() {
         <Definition />
         <Companies />
         <Instruction />
+        <Guide />
+        <Restrictions />
         </div>
     )
 }
