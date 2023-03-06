a0733b4be2ed5b6e2a048254f82c0923324fdd17
Mohammad Davodi <mohammaddavodi96@gmail.com>
2023-03-06 15:40:45 +0330

    Details route done and make it SSG

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -1,7 +1,7 @@
-export default function CardDetails() {
+export default function CardDetails({data}) {
     return(
         <div>
-            details
+            
         </div>
     )
 }
@@ -9,15 +9,22 @@ export default function CardDetails() {
 
 export async function getStaticPaths() {
     const res = await fetch("http://localhost:4000/data");
-    const data = await res.json();
+    const json = await res.json();
+    const data = json.slice(0,10);
 
+    const paths = data.map(food => ({params:{id: food.id.toString()}}))
+
+    return {
+        paths,
+        fallback:true
+    }
 }
 
 export async function getStaticProps(context) {
     const {params} = context;
     const res = await fetch(`http://localhost:4000/data/${params.id}`);
     const data = await res.json();
-    console.log(data)
+
     return{
         props:{data}
     }
